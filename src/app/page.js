313672fc546301f430baad0e64c23890fc163851
Arjun-Vashisht <arjunvashisht17@gmail.com>
2023-10-02 19:40:26 +0530

    install tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1>HOME</h1>
-  )
+  return <h1 className="text-red-500">HOME</h1>;
 }
